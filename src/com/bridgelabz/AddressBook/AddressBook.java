773e3f5d7e44a9df1bcd5faeadce0aec0c11a6e6
SachinGadge13 <gadges14@gmail.com>
2022-10-15 14:14:23 +0530

    Added:Program to count a person by city or state in addresss book..

--- src/com/bridgelabz/AddressBook/AddressBook.java
+++ src/com/bridgelabz/AddressBook/AddressBook.java
@@ -9,6 +9,7 @@ public class AddressBook {
     public static Map<String, Contacts> cityHashMap = new HashMap<String, Contacts>();
     public static Map<String, Contacts> stateHashMap = new HashMap<String, Contacts>();
 
+
     public boolean addContact(Contacts contact) {
         List<Contacts> checkByName = searchByName(contact.getFirstName());
         for (Contacts equalName : checkByName)
@@ -225,6 +226,33 @@ public class AddressBook {
                 return;
             default:
                 System.out.println("INVALID CHOICE!");
+        }
+    }
+
+    //method to count element by option
+    public void countByOption() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Count City ");
+        System.out.println("2. Count State");
+        System.out.println("3. Back ");
+        System.out.println("Enter Your Choice : ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+        switch (choice) {
+            case 1:
+                Map<String, Long> countCity = contactList.stream()
+                        .collect(Collectors.groupingBy(e -> e.getCity(), Collectors.counting()));
+                System.out.println(countCity + "\n");
+                break;
+            case 2:
+                Map<String, Long> countState = contactList.stream()
+                        .collect(Collectors.groupingBy(e -> e.getState(), Collectors.counting()));
+                System.out.println(countState + "\n");
+                break;
+            case 3:
+                return;
+            default:
+                System.out.println("Invalid Option");
         }
     }
 }
--- src/com/bridgelabz/AddressBook/AddressBookMain.java
+++ src/com/bridgelabz/AddressBook/AddressBookMain.java
@@ -15,7 +15,7 @@ public class AddressBookMain {
 
         while (true) {
             System.out.println("\nWelcome to Address Book System");
-            System.out.println("1. New Address Book \n2. Select Address Book \n3. Delete Address Book \n4. Search Contact Data \n5. Exit");
+            System.out.println("1. New Address Book \n2. Select Address Book \n3. Delete Address Book \n4. Search Contact Data \n5.View Contact Data \n6.Count Contacts \n7. Exit");
             System.out.print("Enter Your choice: ");
             int choice = sc.nextInt();
             sc.nextLine();
@@ -54,6 +54,10 @@ public class AddressBookMain {
                     addressBook.viewByOption(addressBookMap);
                     break;
                 case 6:
+                    System.out.println("Welcome to the couter");
+                    addressBook.countByOption();
+                    break;
+                case 7:
                     sc.close();// for closing the programme
                     return;
                 default:
